742fafdc635e5094d76ae4f0650cdec922544999
cris52 <cris52>
2023-07-04 03:46:38 -0400

    Initial commit

---
+++ resultados.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+UUID=""
+CLASE=$1
+TERM=ansi whiptail --title "UNAM FCA RESULTADOS" --infobox "Clase UNAM - $CLASE" 8 78
+
+for(( ; ; ))do
+        echo "Esperando conexion ..."
+ 	UUID=$(./reader-nfc)
+        NAME=$(grep $UUID ./alumnos.csv | awk -F',' '{ print $2 }')
+        LASTNAME=$(grep $UUID ./alumnos.csv | awk -F',' '{ print $3 }')
+        NUMA=$(grep -nr $UUID asistencias | wc -l)
+        NUMP=$(grep -nr $UUID participaciones | wc -l)
+	TERM=ansi whiptail --title "UNAM FCA RESULTADOS" --infobox "Nombre:$NAME $LASTNAME  \nAsistencias: $NUMA  \nParticipaciones: $NUMP" 20 78
+	sleep 10;
+done;
+
+exit 0
